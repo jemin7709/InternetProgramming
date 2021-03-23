5de061b906f075dafe710561b289acb0ad9554de
jemin7709 <jemin7709@naver.com>
2021-03-23 13:07:00 +0900

    4주차

---
+++ 4주차  과제/lib.js
@@ -0,0 +1,6 @@
+function over(obj) {
+	obj.src="https://cdn.mos.cms.futurecdn.net/42E9as7NaTaAi4A6JcuFwG-1200-80.jpg";
+}
+function out(obj) {
+	obj.src="https://t1.daumcdn.net/cfile/tistory/2502973855D2D9702C";
+}
